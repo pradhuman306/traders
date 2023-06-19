ff22d304dbb42d5f60686e8316118b71fe79a54c
pradhuman306 <pradhuman_306>
2023-06-19 10:07:57 +0530

    worked on dashboard page

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -11,58 +11,100 @@ function Admin(props) {
   const user_id = props.auth.userdata.id;
   const dispatch = useDispatch();
   const totalData = useSelector((state) => state.authReducer).totalData;
-  const [totalProfitLoss,setTotalProfitLoss] = useState(0);
-  const [yearList,setYearList] = useState([]);
-  const [yearValue,setYearValue] = useState({label:currentYear,value:currentYear});
-  
-  useEffect(()=>{
-  dispatch(getAllTotalData({user_id:user_id,year:currentYear}));
-  },[])
+  const [totalProfitLoss, setTotalProfitLoss] = useState(0);
+  const [yearList, setYearList] = useState([]);
+  const [yearValue, setYearValue] = useState({
+    label: currentYear,
+    value: currentYear,
+  });
 
+  useEffect(() => {
+    dispatch(getAllTotalData({ user_id: user_id, year: currentYear }));
+  }, []);
 
-  
-
-  useEffect(()=>{
-   let yearList = [{label:currentYear,value:currentYear},{label:2024,value:2024}];
-   let totalProfitLoss = parseInt(totalData.sale)-(parseInt(totalData.buy)+parseInt(totalData.investment)+parseInt(totalData.rent));
-   setTotalProfitLoss(totalProfitLoss);
-   if(totalData.year&&totalData.year.length){
-    totalData.year.map((item)=>{
-      yearList.push({label:item,value:item})
-    })
-   }
-   setYearList(yearList);
-    },[totalData])
-
-    const handleSelectChange = (e) => {
-      dispatch(getAllTotalData({user_id:user_id,year:e.value}));
-      setYearValue(e);
+  useEffect(() => {
+    let yearList = [
+      { label: currentYear, value: currentYear },
+      { label: 2024, value: 2024 },
+    ];
+    let totalProfitLoss =
+      parseInt(totalData.sale) -
+      (parseInt(totalData.buy) +
+        parseInt(totalData.investment) +
+        parseInt(totalData.rent));
+    setTotalProfitLoss(totalProfitLoss);
+    if (totalData.year && totalData.year.length) {
+      totalData.year.map((item) => {
+        yearList.push({ label: item, value: item });
+      });
     }
+    setYearList(yearList);
+  }, [totalData]);
+
+  const handleSelectChange = (e) => {
+    dispatch(getAllTotalData({ user_id: user_id, year: e.value }));
+    setYearValue(e);
+  };
 
   return (
     <>
-
-            <Header heading="Dashboard" {...props} />
+      <Header heading="Dashboard" {...props} />
       <div className="body-content">
         <div className="body-content-wrap">
-        <h1 className="mt-0 mb-1">
-           
-        Hello {titleCase(props.auth.userdata.name)},
-        </h1>
-        <h4 className="sub-heading mb-3">Welcome note</h4>
-        <p>Admin welcome content</p>
-        <p>Total buy:{priceFormatter(totalData.buy)}</p>
-        <p>Total Sell:{priceFormatter(totalData.sale)}</p>
-        <p>Total Investment:{priceFormatter(totalData.investment)}</p>
-        <p>Total Transport:{priceFormatter(totalData.rent)}</p>
-        <p>Total Pending rent:{priceFormatter(totalData.pending_rent)}</p>
-        <p>Total balance amount:{priceFormatter(totalData.balance_amount)}</p>
-        <p className={`${totalProfitLoss<0 ? 'text-bg-danger':'text-bg-success'}`}>
-          {totalProfitLoss<0?'Total Loss ':'Total Profit '}
-          {priceFormatter(makePositive(totalProfitLoss))}
-          </p>
-          <label>Select year</label>
-          <Select
+          <h1 className="mt-0 mb-1">
+            Hello {titleCase(props.auth.userdata.name)},
+          </h1>
+          <div className="desktop-wrap">
+            <div className="desktop-box">
+             <div className="desktop-box-inr">
+             <p>Total buy:</p>
+              <h3>{priceFormatter(totalData.buy)}</h3>
+             </div>
+            </div>
+            <div className="desktop-box">
+            <div className="desktop-box-inr">
+              <p>Total Sell:</p>
+              <h3>{priceFormatter(totalData.sale)}</h3>
+              </div>
+            </div>
+            <div className="desktop-box">
+            <div className="desktop-box-inr">
+              <p>Total Investment:</p>
+              <h3>{priceFormatter(totalData.investment)}</h3>
+              </div>
+            </div>
+            <div className="desktop-box">
+            <div className="desktop-box-inr">
+              <p>Total Transport:</p>
+              <h3>{priceFormatter(totalData.rent)}</h3>
+              </div>
+            </div>
+            <div className="desktop-box">
+            <div className="desktop-box-inr">
+              <p>Total Pending rent:</p>
+              <h3>{priceFormatter(totalData.pending_rent)}</h3>
+              </div>
+            </div>
+            <div className="desktop-box">
+            <div className="desktop-box-inr">
+              <p>Total balance amount:</p>
+              <h3>
+                {priceFormatter(makePositive(totalData.balance_amount))}
+                {totalData.balance_amount > 0 ? " Dr." : " Cr."}
+              </h3>
+              </div>
+            </div>
+            <div className="desktop-box">
+            <div className="desktop-box-inr">
+              <p>{totalProfitLoss < 0 ? "Total Loss " : "Total Profit "}</p>
+              <h3>{priceFormatter(makePositive(totalProfitLoss))}</h3>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <label>Select year</label>
+            <Select
               id="year"
               options={yearList}
               onChange={(e) => handleSelectChange(e)}
@@ -77,7 +119,8 @@ function Admin(props) {
                 },
               })}
             />
-      </div>
+          </div>
+        </div>
       </div>
     </>
   );
